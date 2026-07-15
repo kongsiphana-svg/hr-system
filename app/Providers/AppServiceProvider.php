efad9f32563d143159840e9a172ca9b8c2a1724c
Limmmmm <limloveu3@gmail.com>
2026-07-15 23:00:10 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated; // Add this line!
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force the default guest/RedirectIfAuthenticated redirect to the login page
+        RedirectIfAuthenticated::redirectUsing(function () {
+            return route('login');
+        });
     }
 }
